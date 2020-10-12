1a2c2b1f5ae8ed0a4a5e7c41778b3c0619ba865c
zwelidumsani <zwelidumsani@gmail.com>
2020-10-12 17:01:50 +0200

    Latest changes

--- express/app.js
+++ express/app.js
@@ -3,6 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser')
+var session  = require('express-session')
+var MongoStore  = require('connect-mongo')(session)
+var mongoClient = require('mongoose')
+var flash = require('connect-flash')
+var nodemailer = require('nodemailer')
+var http = require('http');
+var csrf = require('csurf');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,14 +23,27 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+var app = express();
+app.disable('x-powered-by');
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+app.set('view engine', 'hbs');
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.text());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'mySuperSecret',
+ resave: false,
+ saveUninitialized: false,
+ Store: new MongoStore({mongooseConnection: mongoClient.connection}),
+ cookie: {maxAge: 14*24*60*60*1000}
+ }));
+app.use(flash());
+//app.use(require('cookie-parser')(credentials.cookieSecret));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- express/routes/index.js
+++ express/routes/index.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-
-router.get('/about', function(req, res, next){
-	res.send('This is the about us page.');
+  res.render('index.handlebars');
 });
 
 module.exports = router;
