26d6cc11ea2cf5ec7b17a074574f5e416771bc02
zwelidumsani <zwelidumsani@gmail.com>
2020-10-14 13:57:10 +0200

    All pages running on nodejs

--- express/routes/index.js
+++ express/routes/index.js
@@ -6,4 +6,39 @@ router.get('/', function(req, res, next) {
   res.render('index.handlebars');
 });
 
+router.get('/contact', function(req, res, next) {
+  res.render('contacts.handlebars');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about.handlebars');
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services.handlebars');
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('project.handlebars');
+});
+
+router.get('/career', function(req, res, next) {
+  res.render('career.handlebars');
+});
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq.handlebars');
+});
+
+router.get('/team', function(req, res, next) {
+  res.render('team.handlebars');
+});
+
+router.get('/testimonials', function(req, res, next) {
+  res.render('testimonial.handlebars');
+});
+
+
+
+
 module.exports = router;
--- express/routes/users.js
+++ express/routes/users.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/contacts-us', function(req, res, next) {
+  res.render('contacts.handlebars');
+});
 module.exports = router;
